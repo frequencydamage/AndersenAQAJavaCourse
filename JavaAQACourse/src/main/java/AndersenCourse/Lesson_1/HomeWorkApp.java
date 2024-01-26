8b0a299b9e0a18ee0407d2d909189ae88a017a21
Andrej <andrej.novak.v@gmail.com>
2024-01-26 20:39:34 +0100

    .

--- JavaAQACourse/src/main/java/AndersenCourse/Lesson_1/HomeWorkApp.java
+++ JavaAQACourse/src/main/java/AndersenCourse/Lesson_1/HomeWorkApp.java
@@ -1,35 +1,42 @@
 package AndersenCourse.Lesson_1;
 
 public class HomeWorkApp {
-    public static void printThreeWords(){
+    public static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
     }
-    public static void checkSumSign(){
+    public static void checkSumSign() {
         int a = 5;
         int b = -15;
-        if (a + b >= 0)
+        if (a + b >= 0) {
             System.out.println("Сумма положительная");
-        else
+        }
+        else {
             System.out.println("Сумма отрицательная");
+        }
     }
-    public static void printColor(){
+    public static void printColor() {
         int value = 101;
-        if (value <= 0)
+        if (value <= 0) {
             System.out.println("Красный");
-        else if (value > 0 && value <= 100)
+        }
+        else if (value > 0 && value <= 100) {
             System.out.println("Желтый");
-        else if (value > 100)
+        }
+        else if (value > 100) {
             System.out.println("Зеленый");
+        }
     }
-    public static void compareNumbers(){
+    public static void compareNumbers() {
         int a = 33;
         int b = 4;
-        if (a >= b)
+        if (a >= b) {
             System.out.println("a >= b");
-        else
+        }
+        else {
             System.out.println("a < b");
+        }
     }
     public static void main(String[] args) {
         printThreeWords();
