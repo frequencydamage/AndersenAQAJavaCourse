6fced9b69906864c65ee0a063b7618b807c8e48f
Andrej <andrej.novak.v@gmail.com>
2024-02-12 00:25:11 +0100

    Main class that demonstrates app functional.

--- JavaAQACourse/src/main/java/AndersenCourse/lesson_5/fruitsApp/FruitsApp.java
+++ JavaAQACourse/src/main/java/AndersenCourse/lesson_5/fruitsApp/FruitsApp.java
@@ -1,24 +1,19 @@
 package AndersenCourse.lesson_5.fruitsApp;
 
+import java.awt.desktop.AppForegroundListener;
+
 public class FruitsApp {
     public static void main(String[] args) {
-        FruitBox<Apples> applesFruitBox = new FruitBox<>();
-        applesFruitBox.addFruits(new Apples(), 20);
-        FruitBox<Oranges> orangesFruitBox = new FruitBox<>();
-        orangesFruitBox.addFruits(new Oranges(), 0);
-        orangesFruitBox.addFruits(new Oranges(), 25);
-        float boxOfApplesWeight = applesFruitBox.getWeight();
-        float boxOfOrangesWeight = orangesFruitBox.getWeight();
-        System.out.println(boxOfApplesWeight);
-        System.out.println(boxOfOrangesWeight);
-        orangesFruitBox.compareWeights(applesFruitBox);
-        FruitBox<Apples> newApplesFruitBox = new FruitBox<>();
-        applesFruitBox.moveFruits(newApplesFruitBox);
-        newApplesFruitBox.compareWeights(applesFruitBox);
-        boxOfApplesWeight = applesFruitBox.getWeight();
-        System.out.println(boxOfApplesWeight);
-        applesFruitBox.addFruits(new Apples(), 15);
-        applesFruitBox.moveFruits(newApplesFruitBox);
+        FruitBox<Fruits> firstFruitBox = new FruitBox<>();
+        FruitBox<Fruits> secondFruitBox = new FruitBox<>();
+        FruitBox<Fruits> boxToSwap = new FruitBox<>();
+        firstFruitBox.addFruits(new Apples(), 10);
+        firstFruitBox.addFruits(new Oranges(), 3);
+        secondFruitBox.addFruits(new Oranges(), 10);
+        firstFruitBox.compareWeights(secondFruitBox);
+        firstFruitBox.moveFruits(secondFruitBox);
+        firstFruitBox.moveFruits(boxToSwap);
+        firstFruitBox.addFruits(new Oranges(), 15);
     }
 }
 
