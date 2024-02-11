3fef4d71e340a5eee245f660ab9ba7367e2ca5c3
Andrej <andrej.novak.v@gmail.com>
2024-02-11 22:45:28 +0100

    Small cosmetic improvements

--- JavaAQACourse/src/main/java/AndersenCourse/lesson_5/arraySwap/ArraySwapClass.java
+++ JavaAQACourse/src/main/java/AndersenCourse/lesson_5/arraySwap/ArraySwapClass.java
@@ -4,15 +4,15 @@ public class ArraySwapClass {
     public static void main(String[] args) {
         int[] myArray = {5, 15, 4, -32, 146, -9, 55, 66};
 
-        swap(myArray,2, 6);
-        for (Integer i : myArray){
+        swap(myArray, 2, 6);
+        for (Integer i : myArray) {
             System.out.println(i);
         }
     }
 
-    public static void swap(int[] array, int firstIndex, int secondIndex){
+    public static void swap(int[] array, int firstIndex, int secondIndex) {
         int saveValue;
-        if (firstIndex > 0 && firstIndex < array.length && secondIndex > 0 && secondIndex < array.length){
+        if (firstIndex > 0 && firstIndex < array.length && secondIndex > 0 && secondIndex < array.length) {
             saveValue = array[firstIndex];
             array[firstIndex] = array[secondIndex];
             array[secondIndex] = saveValue;
