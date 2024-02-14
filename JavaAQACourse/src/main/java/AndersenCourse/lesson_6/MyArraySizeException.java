010726cbea7145b20d64f961254b125992bd1456
Andrej <andrej.novak.v@gmail.com>
2024-02-14 23:16:57 +0100

    Added a custom exception to handle cases where a method is provided with an array of incorrect size in its parameters.

---
+++ JavaAQACourse/src/main/java/AndersenCourse/lesson_6/MyArraySizeException.java
@@ -0,0 +1,8 @@
+package AndersenCourse.lesson_6;
+
+public class MyArraySizeException extends Exception {
+    @Override
+    public String getMessage() {
+        return "Unappropriate array size!";
+    }
+}
